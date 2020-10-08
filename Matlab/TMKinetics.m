f22d504b0d0abb397cd3e78adc9c70491af8e37f
Dan Feeney <daniel.feeney@colorado.edu>
2020-10-08 17:01:14 -0600

    First commit

---
+++ Matlab/TMKinetics.m
@@ -0,0 +1,135 @@
+%% TM with kinetics 
+clear
+subBW = 85;
+addpath('C:\Users\Daniel.Feeney\Documents\Trail Run\Run Code')
+%[file, path] = uigetfile;
+%kinData = importTMKinetics(strcat(path,file));
+kinData = importTMKinetics('C:\Users\Daniel.Feeney\Dropbox (Boa)\Hike Work Research\Data\TMHike\brett_up_SD - Kinetics.txt');
+forceThresh = 50;
+
+%fnameLong = strsplit(file,'_'); condLong = strsplit(fnameLong{3},'-');
+%subj = fnameLong{1}; cond = condLong{1}(1:2); %splitting the file name to obtain subject and condition
+%% Kinetics and zero'ing all kinetics when left foot is not on belt
+% first, remove low Z force values
+%filt_forceZ = filt_forceZ-35;
+filt_forceZ = kinData.LeftZForce * -1;
+filt_forceZ(filt_forceZ<forceThresh) = 0; %detrend the Z signals so they start at 0
+
+% delimt when there is force on the left side
+leftStrike = zeros(length(filt_forceZ),1);
+leftStrike(find(filt_forceZ)) = 1;
+% element-wise multiple binary leftStrike vector with each joint
+% Powers
+LAnklePower = leftStrike .* kinData.LAnklePower;
+LKneePower = leftStrike .* kinData.LKneePower;
+LHipPower = leftStrike .* kinData.LHipPower;
+% Moments
+LAnkleMomX = leftStrike .* kinData.LAnkleMomentx; LAnkleMomY = leftStrike .* kinData.LAnkleMomenty; LAnkleMomZ = leftStrike .* kinData.LAnkleMomentz;
+LKneeMomX = leftStrike .* kinData.LKneeMomentX; LKneeMomY = leftStrike .* kinData.LKneeMomentY; LKneeMomZ = leftStrike .* kinData.LKneeMomentZ;
+LHipMomX = leftStrike .* kinData.LHipMomentx; LHipMomY = leftStrike .* kinData.LHipMomenty; LHipMomZ = leftStrike .* kinData.LHipMomentz;
+
+%% delimit initial contact and toe off
+ic = zeros(60,1);
+count = 1;
+for step = 1:length(leftStrike)-1
+    if (filt_forceZ(step) == 0 & filt_forceZ(step + 1) > 30)
+        ic(count) = step;
+        count = count + 1;
+    end
+end
+
+to = zeros(60,1);
+count = 1;
+for step = 1:length(leftStrike)-1
+    if (filt_forceZ(step) > 30 & filt_forceZ(step + 1) == 0)
+        to(count) = step;
+        count = count + 1;
+    end
+end
+% trim first contact/toe off if not a full step
+if (ic(1) > to(1))
+   to(1) = []; 
+end
+ic(ic == 0) = []; to(to == 0) = [];
+
+if (ic(end) > to(end))
+   ic(end) = []; 
+end
+% Calculate step lengths
+stepLens = to - ic;
+
+%% extract features from each step length
+
+for step = 1:length(stepLens)
+    %powers
+    maxAnklePow(step) = max(LAnklePower(ic(step):to(step)));
+    maxKneePow(step) = max(LKneePower(ic(step):to(step)));
+    maxHipPow(step) = max(LHipPower(ic(step):to(step)));
+    %work
+    AnkleW(step) = sum(LAnklePower(ic(step):to(step)));
+    KneeW(step) = sum(LKneePower(ic(step):to(step)));
+    HipW(step) = sum(LHipPower(ic(step):to(step)));
+    %Moments
+    AnkleMx(step) = min(LAnkleMomX(ic(step):to(step))); AnkleMy(step) = max(LAnkleMomY(ic(step):to(step))); AnkleMz(step) = min(LAnkleMomZ(ic(step):to(step)));
+    KneeMx(step) = max(LKneeMomX(ic(step):to(step))); KneeMy(step) = max(LKneeMomY(ic(step):to(step))); KneeMz(step) = max(LKneeMomZ(ic(step):to(step)));
+    HipMx(step) = max(LHipMomX(ic(step):to(step))); HipMy(step) = max(LHipMomY(ic(step):to(step))); HipMz(step) = max(LHipMomZ(ic(step):to(step)));
+end
+
+output = [stepLens'; maxAnklePow; maxKneePow; maxHipPow; AnkleW; KneeW; HipW; AnkleMx; AnkleMy; AnkleMz; KneeMx; KneeMy; KneeMz; HipMx; HipMy; HipMz]';
+
+%% plots to visualize.
+figure
+plot(filt_forceZ)
+hold on
+plot(LAnklePower)
+plot(LKneePower)
+plot(LHipPower)
+ylabel('Joint Power (W)')
+title('Joint Powers')
+legend('Force','Ankle','Knee','Hip')
+xlim([200 600])
+
+%% Ankle moments
+figure
+yyaxis left
+plot(LAnkleMomX)
+hold on
+plot(LAnkleMomY)
+plot(LAnkleMomZ)
+yyaxis right
+plot(filt_forceZ)
+title('Ankle Moments')
+ylabel('Joint Moment (Nm)')
+legend('X','Y','Z','Force')
+xlim([200 600])
+%% Knee moments
+figure
+yyaxis left
+plot(LKneeMomX)
+hold on
+plot(LKneeMomY)
+plot(LKneeMomZ)
+yyaxis right
+plot(filt_forceZ)
+title('Knee Moments')
+ylabel('Joint Moment (Nm)')
+legend('X','Y','Z','Force')
+xlim([200 600])
+
+%% Hip moments
+yyaxis left
+plot(LHipMomX)
+hold on
+plot(LHipMomY)
+plot(LHipMomZ)
+yyaxis right
+plot(filt_forceZ)
+title('Hip Moments')
+ylabel('Joint Moment (Nm)')
+legend('X','Y','Z','Force')
+xlim([200 600])
+
+
+
+
+
---
+++ Python/DualBeltAnalysis.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 11:38:57 2020
+
+@author: Daniel.Feeney
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import scipy.signal as sig
+import seaborn as sns
+
+# Define constants and options
+fThresh = 50; #below this value will be set to 0.
+writeData = 0; #will write to spreadsheet if 1 entered
+
+# Read in balance file
+fPath = 'C:/Users/Daniel.Feeney/Dropbox (Boa)/EnduranceProtocolWork/WalkData/'
+entries = os.listdir(fPath)
+
+# list of functions 
+# finding landings on the force plate once the filtered force exceeds the force threshold
+def findLandings(force):
+    lic = []
+    for step in range(len(force)-1):
+        if force[step] == 0 and force[step + 1] >= fThresh:
+            lic.append(step)
+    return lic
+
+#Find takeoff from FP when force goes from above thresh to 0
+def findTakeoffs(force):
+    lto = []
+    for step in range(len(force)-1):
+        if force[step] >= fThresh and force[step + 1] == 0:
+            lto.append(step + 1)
+    return lto
+
+def trimLandings(landings, takeoffs):
+    trimTakeoffs = landings
+    if len(takeoffs) > len(landings) and takeoffs[0] > landings[0]:
+        del(trimTakeoffs[0])
+    return(trimTakeoffs)
+
+def trimTakeoffs(landings, takeoffs):
+    if len(takeoffs) < len(landings):
+        del(landings[-1])
+    return(landings)
+
+
+def calcVLR(force, startVal, lengthFwd):
+    # function to calculate VLR from 80 and 20% of the max value observed in the first n
+    # indices (n defined by lengthFwd). 
+    maxF = np.max(forceZ[startVal:startVal+lengthFwd])
+    eightyPctMax = 0.8 * maxF
+    twentyPctMax = 0.2 * maxF
+    # find indices of 80 and 20 and calc loading rate as diff in force / diff in time (N/s)
+    eightyIndex = next(x for x, val in enumerate(force[startVal:startVal+lengthFwd]) 
+                      if val > eightyPctMax) 
+    twentyIndex = next(x for x, val in enumerate(force[startVal:startVal+lengthFwd]) 
+                      if val > twentyPctMax) 
+    VLR = ((eightyPctMax - twentyPctMax) / ((eightyIndex/1000) - (twentyIndex/1000)))
+    return(VLR)
+    
+#Find max braking force moving forward
+def calcPeakBrake(force, landing, length):
+    newForce = np.array(force)
+    return min(newForce[landing:landing+length])
+
+def findNextZero(force, length):
+    # Starting at a landing, look forward (after first 15 indices)
+    # to the find the next time the signal goes from - to +
+    for step in range(length):
+        if force[step] <= 0 and force[step + 1] >= 0 and step > 15:
+            break
+    return step
+
+
+#Preallocation
+loadingRate = []
+peakBrakeF = []
+brakeImpulse = []
+VLR = []
+VLRtwo = []
+sName = []
+tmpConfig = []
+timeP = []
+NL = []
+
+# Save Time series data in separate DF #
+vertForce = []
+longConfig = []
+longSub = []
+timeIndex = []
+
+## loop through the selected files
+for file in entries:
+    try:
+        
+        fName = file #Load one file at a time
+        
+        dat = pd.read_csv(fPath+fName,sep='\t', skiprows = 8, header = 0)
+        #Parse file name into subject and configuration 
+        subName = fName.split(sep = "_")[0]
+        config = fName.split(sep = "_")[2]
+        timePoint = fName.split(sep = "_")[3]
+        
+        # Filter force
+        forceZ = dat.RForceZ * -1
+        forceZ[forceZ<fThresh] = 0
+        brakeForce = dat.RForceY[0:59999] * -1
+        
+        
+        fs = 1000 #Sampling rate
+        t = np.arange(59999) / fs
+        fc = 20  # Cut-off frequency of the filter
+        w = fc / (fs / 2) # Normalize the frequency
+        b, a = sig.butter(4, w, 'low')
+        brakeFilt = sig.filtfilt(b, a, brakeForce)
+        
+        #find the landings and offs of the FP as vectors
+        landings = findLandings(forceZ)
+        takeoffs = findTakeoffs(forceZ)
+                
+        ### to delete ###
+        
+        #plt.plot(forceZ[landings[10]:landings[10]+800])
+
+        #plt.plot(brakeFilt[landings[10]:landings[10]+1000])
+        #sum(brakeFilt[landings[10]+10:landings[10]+400])
+        ## delete ##
+        
+        #For each landing, calculate rolling averages and time to stabilize
+    
+        for landing in landings:
+            try:
+               # Define where next zero is
+                nextLanding = findNextZero(brakeFilt[landing:landing+600],600)
+                NL.append(nextLanding)
+                #stepLen.append(findStepLen(forceZ[landing:landing+800],800))
+                brakeImpulse.append(sum(brakeFilt[landing+10:landing+nextLanding]))
+                sName.append(subName)
+                tmpConfig.append(config)
+                timeP.append(timePoint)
+                peakBrakeF.append(calcPeakBrake(brakeFilt,landing, 600))
+                VLR.append(calcVLR(forceZ, landing, 200))
+                
+                ## Time series Saving ##
+                timeIndex.append(np.arange(0,800))
+                vertForce.append(forceZ[landing:landing+800])
+                longConfig.append(np.repeat(config, 800))
+                longSub.append(np.repeat(subName,800))
+            except:
+                print(landing)
+        
+    except:
+            print(file)
+
+outcomes = pd.DataFrame({'Sub':list(sName), 'Config': list(tmpConfig), 'TimePoint':list(timeP),'NL':list(NL),'peakBrake': list(peakBrakeF),
+                         'brakeImpulse': list(brakeImpulse), 'VLR': list(VLR)})
+
+df2 = pd.DataFrame(pd.concat(vertForce), np.concatenate(longConfig))    
+
+#longDat = pd.concat(vertForce, ignore_index = True)
+#longDat2['Config'] = pd.DataFrame(np.concatenate(longConfig))
+#longDat['Sub'] = pd.DataFrame(np.concatenate(longSub))
+#longDat['TimePoint'] = pd.DataFrame(np.concatenate(timeIndex))
+
+outcomes[['peakBrake']] = -1 * outcomes[['peakBrake']]
+cleanedOutcomes = outcomes[outcomes['brakeImpulse'] <= -1000]
+cleanedOutcomes[['brakeImpulse']] = -1 * cleanedOutcomes[['brakeImpulse']]
+    
+
+f, axes = plt.subplots(1,4)
+sns.boxplot(y='peakBrake', x='Sub', hue="Config",
+                 data=outcomes, 
+                 palette="colorblind", ax=axes[0])
+
+sns.boxplot(y='VLR', x='Sub', hue = "Config", 
+                 data=outcomes, 
+                 palette="colorblind", ax=axes[1])
+
+sns.boxplot(y='brakeImpulse', x='Sub', hue = "Config", 
+                 data=cleanedOutcomes, 
+                 palette="colorblind", ax=axes[2])
+
+sns.boxplot(y='NL', x='Sub', hue = "Config", 
+                 data=cleanedOutcomes, 
+                 palette="colorblind", ax=axes[3])
+plt.tight_layout()
+
+newForce = pd.concat(vertForce)
+newSub = pd.concat(longSub)
+newConfig = pd.concat(longConfig)
+newTry = {'vertForce': newForce,
+        'Sub': newSub,
+        'Config': newConfig
+        }
+
+df = pd.DataFrame(newTry, columns = ['Force', 'Sub','Config'])
+### 
+#fmri = sns.load_dataset("fmri")
+#sns.relplot(
+#    data=fmri, kind="line",
+#    x="timepoint", y="signal", col="region",
+#    hue="event", style="event",
+#)
+
+sns.relplot(
+        data = vertForce, kind = "line")
+
+###
