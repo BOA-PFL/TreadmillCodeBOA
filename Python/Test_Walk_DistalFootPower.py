38489f315f72a4cb53e17f9576c36195fe009366
EricHonertBOA <eric.honert@boatechnology.com>
2022-09-07 15:48:28 -0600

    Update Test_Walk_DistalFootPower.py

--- Python/Test_Walk_DistalFootPower.py
+++ Python/Test_Walk_DistalFootPower.py
@@ -18,19 +18,11 @@ import scipy
 
 
 # Define constants and options
-run = 1 # Set this to 1 where participant is running on one belt so only the left are detected. 0 for dual belt
-manualTrim = 0  #set this to 1 if you want to manually trim trials with ginput, 0 if you want it auto trimmed (start and end of trial)
 fThresh = 50 #below this value will be set to 0.
 writeData = 0 #will write to spreadsheet if 1 entered
-plottingEnabled = 0 #plots the bottom if 1. No plots if 0
 lookFwd = 50
 timeToLoad = 75 #length to look forward for an impact peak
 pd.options.mode.chained_assignment = None  # default='warn' set to warn for a lot of warnings
-save_on = 0 # Turn to 1 to save outcomes to csv
-
-# Look at the text files from the foot work 
-fPath_footwork = 'C:\\Users\\eric.honert\\Boa Technology Inc\\PFL Team - General\\Testing Segments\\WorkWear_Performance\\Elten_Jan2022\\Treadmill\FootPower\\'
-entries_footwork = os.listdir(fPath_footwork)
    
 #______________________________________________________________________________    
 # list of functions 
@@ -151,34 +143,37 @@ def trimTakeoffs(landingVec, takeoffVec):
         return(takeoffVec)
     
 
-def dist_seg_power_treadmill(inputDF,speed,slope,landings,takeoffs):
+def dist_seg_power_treadmill(Seg_COM_Pos,Seg_COM_Vel,Seg_Ang_Vel,CenterOfPressure,GRF,FreeMoment,speed,landings,takeoffs,yn_run):
     """
     The purpose of this function is to compute the distal segment power -
     commonly applied to the rearfoot to obtain the distal rearfoot power. The
     power in this formation was provided in Takahashi et al. 2012; but
     originally in Siegel et al. 1996. For full derivations, see Zelik and 
-    Honert 2018 appendix. This code assumes that the walking direction is +y
+    Honert 2018 appendix. This code assumes that the locomotion direction is +y
 
     Parameters
     ----------
-    inputDF : DataFrame
-        This data frame needs to contain the following variables:
-            Seg_COM_Vel = Segment COM Velocity (ex: Foot COM Velocity)
-            Seg_Ang_Vel = Segment Angular Velocity (ex: Foot Angular Velocity)
-            Seg_COM_Pos = Segment COM Position (ex: Foot COM Position)
-            CenterOfPressure = Location of the center of pressure
-            FreeMoment = Free moment on force platform
-            GRF = Ground Reaction Force. Ensure that the input GRF is the REACTION
-            force; whereas, motion monitor exports the action force
+    Seg_COM_Pos : numpy array (N X 3)
+        Segment COM Position (ex: Foot COM Position)
+    Seg_COM_Vel : numpy array (N X 3)
+        Segment COM Velocity (ex: Foot COM Velocity)
+    Seg_Ang_Vel : numpy array (N X 3)
+        Segment Angular Velocity (ex: Foot Angular Velocity)
+    CenterOfPressure : numpy array (N X 3)
+        Location of the center of pressure
+    GRF : numpy array (N X 3)
+        Ground Reaction Force. Ensure that the input GRF is the REACTION
+    FreeMoment : numpy array (N X 3)
+        Free moment on force platform
     speed : float
         Treadmill belt speed - can be used as a debugging variable or to set
         the speed of the foot in 3D space. 
-    slope : int or float
-        Slope of the treadmill
     landings : list
-        Initial foot contact
+        Initial foot contact: used only during walking
     takeoffs : list
-        Or toe-offs
+        Or toe-offs: used only during walking
+    yn_run : int
+        1 for running, 0 for walking
     
     Returns
     -------
@@ -186,43 +181,43 @@ def dist_seg_power_treadmill(inputDF,speed,slope,landings,takeoffs):
         distal rearfoot power
 
     """
-    # Make Variables NumPy Arrays for matrix opperations
-    Seg_Ang_Vel = np.array(list(zip(inputDF.FootAngVel_X,inputDF.FootAngVel_Y,inputDF.FootAngVel_Z)))*(np.pi/180)    
-    Seg_COM_Pos = np.array(list(zip(inputDF.FootCOMPos_X,inputDF.FootCOMPos_Y,inputDF.FootCOMPos_Z)))
-    Seg_COM_Vel = np.array(list(zip(inputDF.FootCOMVel_X,inputDF.FootCOMVel_Y,inputDF.FootCOMVel_Z)))
-    CenterOfPressure = np.array(list(zip(inputDF.COP_X,inputDF.COP_Y,inputDF.COP_Z)))
-    GRF = np.array(list(zip(inputDF.GRF_X,inputDF.GRF_Y,inputDF.GRF_Z)))
-    # Note: The free moment (like the GRF) is needs to be negated in order to
-    # be the reaction free moment
-    FreeMoment = -1*np.array(list(zip(inputDF.FMOM_X,inputDF.FMOM_Y,inputDF.FMOM_Z)))
-    # Debugging variable to examine foot speed
-    debug = 0
-    # When using a treadmill is used for locomotion and the distal segment
-    # power is computed, the treadmill belt speed needs to be taken into
-    # account. Based on prior experience, DURING WALKING, foot flat can provide
-    # a decent approximation of the treadmill belt speed. 
-    foot_flat = [0.2,0.4]
     
-    # Allocate variables
-    step_speed = np.zeros((len(landings)-1,1))
+    # If walking: compute the treadmill belt speed
+    if yn_run == 0:
+        # Debugging variable to examine foot speed
+        debug = 0
+        # When using a treadmill is used for locomotion and the distal segment
+        # power is computed, the treadmill belt speed needs to be taken into
+        # account. Based on prior experience, DURING WALKING, foot flat can provide
+        # a decent approximation of the treadmill belt speed. 
+        foot_flat = [0.2,0.4]
+        
+        # Allocate variables
+        step_speed = np.zeros((len(landings)-1,1))
+        
+        # Index through the landings
+        for ii in range(len(landings)-1):
+            stepframe = takeoffs[ii]-landings[ii]
+            # Frames to analyze based on the foot flat percentages
+            FFframes = range(landings[ii]+round(foot_flat[0]*stepframe),landings[ii]+round(foot_flat[1]*stepframe),1)
+            step_speed[ii] = np.mean(Seg_COM_Vel[FFframes,1])
     
-    # Index through the landings
-    for ii in range(len(landings)-1):
-        stepframe = takeoffs[ii]-landings[ii]
-        # Frames to analyze based on the foot flat percentages
-        FFframes = range(landings[ii]+round(foot_flat[0]*stepframe),landings[ii]+round(foot_flat[1]*stepframe),1)
-        step_speed[ii] = np.mean(inputDF.FootCOMVel_Y[FFframes])
-
-    # Find the average treadmill belt speed of the trial (also exclude any 
-    # zeros in the estimate)
-    avg_speed = -np.mean(step_speed[step_speed != 0])
-    
-    if debug == 1:
-        plt.figure(1010)
-        plt.plot(step_speed)
+        # Find the average treadmill belt speed of the trial (also exclude any 
+        # zeros in the estimate)
+        avg_speed = -np.mean(step_speed[step_speed != 0])
+        
+        if debug == 1:
+            plt.figure(1010)
+            plt.plot(step_speed)
+            
+    # If running: use the inputted Bertec treadmill speed
+    else: 
+        # It is difficult to compute the belt speed from running - thus rely on
+        # the set treadmill belt speed
+        avg_speed = np.array(speed)
     
     # Treadmill belt speed: will need to be updated based on the slope
-    belt_vel = np.array(list(zip([0]*len(inputDF.FootCOMVel_X),[avg_speed]*len(inputDF.FootCOMVel_Y),[0]*len(inputDF.FootCOMVel_Z))))
+    belt_vel = np.array(list(zip([0]*len(Seg_COM_Vel),[avg_speed]*len(Seg_COM_Vel),[0]*len(Seg_COM_Vel))))
     
     # Adjust the segment velocity based on belt speed
     adj_Seg_COM_Vel = Seg_COM_Vel+belt_vel
@@ -299,72 +294,77 @@ def crop_strides_fft(var,landings):
     
     
     
-    
+#______________________________________________________________________________
+save_on = 0 # Turn to 1 to save outcomes to csv
+
+# Look at the text files from the foot work 
+fPath_footwork = 'C:\\Users\\eric.honert\\Boa Technology Inc\\PFL Team - General\\Testing Segments\\WorkWear_Performance\\Jalas_July2022\\FootPower\\'
+entries_footwork = [fName for fName in os.listdir(fPath_footwork) if fName.endswith('.txt')]
 
 #______________________________________________________________________________
-
 #Preallocate variables for storage
 Subject = []
 Config = []
 DisWork = []
+SwingForce = []
 
 
 #______________________________________________________________________________
-avgAnkPow = np.zeros((101,len(entries_footwork)))
 avgFootPow = np.zeros((101,len(entries_footwork)))
 
-# when COPx is more negative, that is left foot strike
-## loop through the selected files
-for ii in range(len(entries_footwork)):
-    try:
-        
-        #______________________________________________________________________
-        # Load the files associated with the foot power/work
-        fName = entries_footwork[ii] #Load one file at a time
-        
-        #Parse file name into subject and configuration 
-        subName = fName.split(sep = "_")[0]
-        ConfigTmp = fName.split(sep = "_")[2]
+for ii in range(0,len(entries_footwork)):
+    fName = entries_footwork[ii] #Load one file at a time
+    #Parse file name into subject and configuration 
+    subName = fName.split(sep = "_")[0]
+    ConfigTmp = fName.split(sep = "_")[2]
+    
+    print('Processing:' + entries_footwork[ii])
+    #______________________________________________________________________
+    # Load the data
+    dat = pd.read_csv(fPath_footwork+fName,sep='\t', skiprows = 8, header = 0)  
+    dat = dat.fillna(0)
 
-        dat = pd.read_csv(fPath_footwork+fName,sep='\t', skiprows = 8, header = 0)  
-        dat = dat.fillna(0)
-        #______________________________________________________________________
-        # Flip the direction of the GRFs
-        dat.GRF_X = -1 * dat.GRF_X
-        dat.GRF_Y = -1 * dat.GRF_Y
-        dat.GRF_Z = -1 * dat.GRF_Z
-
-        # Zero the forces below the threshold
-        forceZ = trimForce(dat, fThresh)
+    # Zero the forces below the threshold
+    ForceZog = np.array(dat.GRF_Z)
+    dat.GRF_Z = trimForce(dat, fThresh)
+    
+    # Find the landings and takeoffs of the FP as vectors
+    landings = findLandings(dat.GRF_Z)
+    takeoffs = findTakeoffs(dat.GRF_Z)
+    # Crop the landings and takeoffs
+    takeoffs = trimTakeoffs(landings, takeoffs)
+    if len(takeoffs) < len(landings):
+        landings = landings[0:-1]
+    
+    # Set up signals for computation
+    Foot_Ang_Vel = np.array(list(zip(dat.RFootAngVel_X,dat.RFootAngVel_Y,dat.RFootAngVel_Z)))*(np.pi/180)    
+    Foot_COM_Pos = np.array(list(zip(dat.RFootCOMPos_X,dat.RFootCOMPos_Y,dat.RFootCOMPos_Z)))
+    Foot_COM_Vel = np.array(list(zip(dat.RFootCOMVel_X,dat.RFootCOMVel_Y,dat.RFootCOMVel_Z)))
+    COP = np.array(list(zip(dat.COP_X,dat.COP_Y,dat.COP_Z)))
+    GRF = np.array(list(zip(dat.GRF_X,dat.GRF_Y,dat.GRF_Z)))
+    FMOM = np.array(list(zip(dat.FMOM_X,dat.FMOM_Y,dat.FMOM_Z)))
+    
+    
+    
+    DFootPower = dist_seg_power_treadmill(Foot_COM_Pos,Foot_COM_Vel,Foot_Ang_Vel,COP,GRF,FMOM,-1.2,landings,takeoffs,0)
+    plt.figure(ii)
+    plt.plot(intp_strides(DFootPower,landings))
+    
+    avgFootPow[:,ii] = np.mean(intp_strides(DFootPower,landings),axis = 1)
+    
+    # for counterVar, landing in enumerate(landings):
+    #     # Note this disipation work is only for walking. For running compute the
+    #     # total negative work
+    #     dis_idx = round((takeoffs[counterVar]-landing)*.20)+landing
+    #     if abs(np.max(DFootPower[landing:takeoffs[counterVar]])) < 400 and np.mean(DFootPower[landing:dis_idx]) < 0 :
+    #         dis_work = scipy.integrate.trapezoid(DFootPower[landing:dis_idx],dx = 1/200)
+            
+    #         if dis_work < -5:
+    #             print('oops')
         
-        # Find the landings and takeoffs of the FP as vectors
-        landings = findLandings(forceZ)
-        takeoffs = findTakeoffs(forceZ)
-        # Crop the landings and takeoffs
-        takeoffs = trimTakeoffs(landings, takeoffs)
-        if len(takeoffs) < len(landings):
-            landings = landings[0:-1]
-        
-        # dum = np.gradient(dat.Foot4_Z)
-        # [sigFFT,XX] = crop_strides_fft(dat.FootAngVel_Y,landings)
-        
-        # Compute the distal rearfoot power:
-        DFootPower = dist_seg_power_treadmill(dat,1.2,0,landings,takeoffs)
-        
-        avgFootPow[:,ii] = np.mean(intp_strides(DFootPower,landings),axis = 1)
-        
-        for counterVar, landing in enumerate(landings):
-            try:
-                dis_idx = round((takeoffs[counterVar]-landing)*.20)+landing
-                dis_work = scipy.integrate.trapezoid(DFootPower[landing:dis_idx],dx = 1/200)
-                
-                DisWork.append(dis_work)
-                Subject.append(subName)
-                Config.append(ConfigTmp)
-            except:
-                print(fName, landing)
-    except:
-        print(fName)
+    #         DisWork.append(dis_work)
+    #         Subject.append(subName)
+    #         Config.append(ConfigTmp)
         
 outcomes = pd.DataFrame({'Subject':list(Subject), 'Config': list(Config),'DisWork': list(DisWork)})
 
