a4abc5d176bad1fc31caba3565088ed9e901fd8a
EricHonertBOA <eric.honert@boatechnology.com>
2025-05-28 17:00:44 -0600

    Update PerformanceTest_CyclingWahoo.py

--- Python/PerformanceTest_CyclingWahoo.py
+++ Python/PerformanceTest_CyclingWahoo.py
@@ -27,26 +27,19 @@ fPath = 'C:\\Users\\eric.honert\\Boa Technology Inc\\PFL Team - General\\Testing
 fileExt = r".fit"
 entries = [fName for fName in os.listdir(fPath) if fName.endswith(fileExt)]
 
-#### Wahoo data
-
-print('Opening all Wahoo .fit files recorded for the subject')
 # Initiate storing variables
 config = []
 subName = []
 Order = []
-steadyPower = []
-steadyCadence = []
-steadySym = []
-
-sprintMax = []
-cadenceMax = []
-sprintSym = []
-
+Movement = []
+avgPower = []
+maxPower = []
+avgCadence = []
+maxCadence = []
 
 # Index through the files selected
 for fName in entries:
-    # If the loop is not able accomplished, "try" will skip the file
-    # try:   
+    # If the loop is not able accomplished, "try" will skip the file   
       #__________________________________________________________________________
       # Extract the fitfile information into dataframe (df)
       print(fName)
@@ -65,9 +58,7 @@ for fName in entries:
           workout.append(r)
       dat = pd.DataFrame(workout)
       #________________________________________________________________________
-      # Create time-continuous power figure to select regions of interest
-      
-       
+      # Create time-continuous power figure to select regions of interest 
       saveFolder = fPath + 'PowerPlots'
        
       if os.path.exists(saveFolder) == False:
@@ -87,10 +78,10 @@ for fName in entries:
           plt.savefig(saveFolder + '/' + fName.split('.csv')[0] +'.png')
           
           print('click the start of as many steady state periods are recorded in the file. Press enter when done')
-          steadyStart = plt.ginput(-1)
+          steadyStart = plt.ginput(-1, timeout = 60)
         
           print('click the start of as many sprints are recorded in the file. Press enter when done')
-          sprintStart = plt.ginput(-1)
+          sprintStart = plt.ginput(-1, timeout = 60)
           plt.close()
           
           pts = [steadyStart,sprintStart]
@@ -103,44 +94,49 @@ for fName in entries:
         
         (ss, y) = steadyStart[k]
         ss = round(ss)
-        steadyPower.append(np.mean(dat.power[ss:ss + ss_dur]))
-        steadyCadence.append(np.mean(dat.cadence[ss:ss + ss_dur]))
+        avgPower.append(np.mean(dat.power[ss:ss + ss_dur]))
+        avgCadence.append(np.mean(dat.cadence[ss:ss + ss_dur]))
+        maxPower.append(np.max(dat.power[ss:ss + ss_dur]))
+        maxCadence.append(np.max(dat.cadence[ss:ss + ss_dur]))
+        Movement.append('SteadyState')
+        subName.append(fName.split('_')[0])
+        config.append(fName.split('_')[1])
+        Order.append(fName.split('_')[2].split('.')[0])
     
       # Index through the steady-state regions and extract metrics of interest
       for j in range(len(sprintStart)):
         sprintPower = [] 
         sprintCadence = []
-        
+                
         (sps, y) = sprintStart[0]
         sps = round(sps)
         for p in range(0,11):
-            # Create a moving average
+            # Create a moving average over the 10 sec sprint
             sprintPower.append(np.mean(dat.power[sps + p: sps + p + 5]))
             sprintCadence.append(np.mean(dat.cadence[sps + p:sps + p + 5]))
         
         # Find the maximum from the moving averaged sprint data
-        sprintMax.append(max(sprintPower))
-        Max_idx = sprintPower.index(sprintMax[-1])
-        cadenceMax.append(sprintCadence[Max_idx])
-
-        # if meter == 0:
-        #  symmetryMax = sprintSym[Max_idx]
-
-      subName.append(fName.split('_')[0])
-      config.append(fName.split('_')[1])
-      Order.append(fName.split('_')[2].split('.')[0])
-
-    
-    # except:
-    #     print(fName)
+        maxPower.append(max(sprintPower))
+        Max_idx = sprintPower.index(maxPower[-1])
+        maxCadence.append(sprintCadence[Max_idx])
+        avgPower.append(np.mean(sprintPower))
+        avgCadence.append(np.mean(sprintCadence))
+        
+        Movement.append('Sprint')
+        subName.append(fName.split('_')[0])
+        config.append(fName.split('_')[1])
+        Order.append(fName.split('_')[2].split('.')[0])
 
 
 
 # Combine outcomes and export to csv
-outcomes = pd.DataFrame({'Subject':list(subName), 'Config': list(config), 'Order':list(Order), 'Power_steady':steadyPower, 'Cadence_steady': steadyCadence, 
-                            'Power_sprint':sprintMax, 'Cadence_sprint':cadenceMax})  
+outcomes = pd.DataFrame({'Subject':list(subName), 'Config': list(config), 'Order':list(Order), 'Movement':list(Movement),
+                         'avgPower':list(avgPower),'avgCadence':list(avgCadence),
+                         'peakPower':list(maxPower), 'peakCadence':list(maxCadence)})  
 
+outfileName = fPath + '0_CompiledPowerData.csv'
 if save_on == 1:
-    outfileName = fPath + '0_CompiledPowerData.csv'
-    outcomes.to_csv(outfileName, header=True, index = False)
-
+    if os.path.exists(outfileName) == False:
+        outcomes.to_csv(outfileName, header=True, index = False)
+    else:
+        outcomes.to_csv(outfileName, mode='a', header=False, index = False)
